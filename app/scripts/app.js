e1e2fea657390c617ecf9741305063f090646b42
Jon Saints <saintsjd@gmail.com>
2013-09-18 14:41:59 -0600

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+/*global define */
+define([], function () {
+    'use strict';
+
+	$('.filter-btn a').click(function(){
+		
+		var whichBtn = $(this).attr('id').split('-')[0];
+		var alreadyActive = $(this).parent().hasClass('active');
+		var filterOpen = $('.filter-btn.active').length;
+
+
+		$('.filter-btn').removeClass('active');
+		$('.filter').slideUp(function(){
+			console.log('up');
+		});
+
+		if( !alreadyActive ){
+			$('#' + whichBtn + '-filter-btn').parent().addClass('active');
+			$('#' + whichBtn + '-filter').slideDown('slow');
+			filterOpen = true;
+		}
+
+
+		return false;
+	});
+
+	$('.update-btn').click(function(){
+
+		$('.filter-btn').removeClass('active');
+		$('.filter').slideUp();
+		return false;
+	});
+
+
+    return '\'Allo \'Allo!';
+});
